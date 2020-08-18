4929b735fe5d0d3c3af641b3107bd93928ccaece
Dinu SV <mail@dinusv.com>
2020-08-18 12:16:05 +0300

    Fixed moxygen path.

--- run_moxygen.js
+++ run_moxygen.js
@@ -5,7 +5,7 @@ console.log("\nExecuting moxygen in " + outpath);
 console.log("-----------------------------------------------------------\n");
 
 // Decoding %20 as space
-child_process.execSync('node live-doc-moxygen.js ' + outpath.replace(" ", "%20"), {
+child_process.execSync('node ' + __dirname + '/live-doc-moxygen.js ' + outpath.replace(" ", "%20"), {
     stdio: [0, 1, 2]
 });
 
